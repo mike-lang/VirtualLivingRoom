ec0864df85406a73ee27b728b95665c153208a46
Michael Lang <mike_lang1010@yahoo.noroboemailsplease>
2012-06-30 19:47:10 -0500

    Converted GetCursor method to JsonP

--- SyncService/SyncService/Controllers/SyncController.cs
+++ SyncService/SyncService/Controllers/SyncController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Ninject;
+using SyncService.Misc;
 using SyncService.Providers;
 
 namespace SyncService.Controllers
@@ -31,6 +32,7 @@ namespace SyncService.Controllers
             return Json(new {reponse = "OK"});
         }
 
+        [JsonpFilter]
         public ActionResult GetCursor()
         {
             var currentPosition = SyncProvider.GetCursor();
